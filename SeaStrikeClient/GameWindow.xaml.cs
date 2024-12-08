e82b8af48a79ff5ba83351b718e15bad3eea264e
ddaniel-bit <danko8651@gmail.com>
2024-12-08 23:13:46 +0100

    valami van e meg mindig szar

---
+++ SeaStrikeClient/GameWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SeaStrikeClient
+{
+    public partial class GameWindow : Window
+    {
+        private readonly TcpClient _client;
+        private readonly NetworkStream _stream;
+        private int[,] _gridMatrix;
+        private Thread _listenThread;  // Hozzáadtunk egy szálat a szerver figyeléséhez
+
+        public GameWindow(TcpClient client, NetworkStream stream)
+        {
+            InitializeComponent();
+            _client = client;
+            _stream = stream;
+
+            // Indítsunk el egy külön szálat a szerver figyelésére
+            _listenThread = new Thread(ListenToServer);
+            _listenThread.IsBackground = true; // Hogy ha az ablak bezárul, a szál is leálljon
+            _listenThread.Start();
+            SendTextMessage();
+        }
+        public async void SendTextMessage()
+        {
+            string message = "alive";
+            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+            await _stream.WriteAsync(messageBytes, 0, messageBytes.Length);
+        }
+        private void ListenToServer()
+        {
+            byte[] buffer = new byte[1024];
+            while (true)
+            {
+                try
+                {
+                    int bytesRead = _stream.Read(buffer, 0, buffer.Length);
+                    if (bytesRead > 0)
+                    {
+                        string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                        if (message == "yourturn")
+                        {
+                            Dispatcher.Invoke(() => // A UI szálon történő hívás szükséges
+                            {
+                                MessageBox.Show("en");
+                            });
+                        } else if (message == "test")
+                        {
+                            MessageBox.Show("test");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Dispatcher.Invoke(() =>
+                    {
+                        MessageBox.Show($"Hiba történt a szerver üzenetének fogadása közben: {ex.Message}");
+                    });
+                    break;
+                }
+            }
+        }
+
+    }
+}
--- SeaStrikeClient/LayoutEditor.xaml.cs
+++ SeaStrikeClient/LayoutEditor.xaml.cs
@@ -1,8 +1,11 @@
-﻿using System.Linq;
+﻿using System;
+using System.IO;
 using System.Net.Sockets;
+using System.Text;
+using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
+using System.Windows.Controls;
 using System.Windows.Media;
 
 namespace SeaStrikeClient
@@ -12,6 +15,7 @@ namespace SeaStrikeClient
         private readonly TcpClient _client;
         private readonly NetworkStream _stream;
         private int[,] _gridMatrix;
+        private Thread _listenThread;  // Hozzáadtunk egy szálat a szerver figyeléséhez
 
         public LayoutEditor(TcpClient client, NetworkStream stream)
         {
@@ -20,6 +24,11 @@ namespace SeaStrikeClient
             _stream = stream;
 
             InitializeMatrix();
+
+            // Indítsunk el egy külön szálat a szerver figyelésére
+            _listenThread = new Thread(ListenToServer);
+            _listenThread.IsBackground = true; // Hogy ha az ablak bezárul, a szál is leálljon
+            _listenThread.Start();
         }
 
         // Mátrix inicializálása
@@ -63,7 +72,56 @@ namespace SeaStrikeClient
         private void FinishButton_Click(object sender, RoutedEventArgs e)
         {
             // Mátrix állapotának ellenőrzése
-            MessageBox.Show("2D Mátrix állapota:\n" + MatrixToString(_gridMatrix));
+            if (HajokSzama(_gridMatrix) == 20)
+            {
+                // Mátrix egy sorba rendezése
+                string formattedMatrix = EgySorbaRendezes(_gridMatrix);
+
+                // Mátrix elküldése a szervernek
+                byte[] data = System.Text.Encoding.UTF8.GetBytes(formattedMatrix);
+                _stream.Write(data, 0, data.Length);
+                _stream.Flush();
+
+                //asd
+                btnStart.Visibility = Visibility.Collapsed;
+                lblWaiting.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                MessageBox.Show("A hajók száma nem megfelelő! Pontosan 20 hajót kell elhelyezni.");
+            }
+        }
+
+        // Mátrix egy sorba rendezése
+        private string EgySorbaRendezes(int[,] matrix)
+        {
+            string sor = "";
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                sor += "{";
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    sor += matrix[i, j];
+                    if (j < matrix.GetLength(1) - 1) sor += ","; // Elemszétválasztó vessző
+                }
+                sor += "}";
+                if (i < matrix.GetLength(0) - 1) sor += ","; // Sorok közötti vessző
+            }
+            return sor;
+        }
+
+        // Segédmetódus a hajók számának kiszámításához
+        private int HajokSzama(int[,] matrix)
+        {
+            int ossz = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    ossz += matrix[i, j];
+                }
+            }
+            return ossz;
         }
 
         // Segédmetódus a mátrix karakterláncként való megjelenítéséhez
@@ -83,6 +141,42 @@ namespace SeaStrikeClient
             }
 
             return result;
+        }
+
+        // Szerver üzenetek figyelése külön szálban
+        private void ListenToServer()
+        {
+            byte[] buffer = new byte[1024];
+            while (true)
+            {
+                try
+                {
+                    int bytesRead = _stream.Read(buffer, 0, buffer.Length);
+                    if (bytesRead > 0)
+                    {
+                        string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                        // Ha "gamestart" üzenetet kaptunk, jelenítsük meg a MessageBox-ot
+                        if (message == "gamestart")
+                        {
+                            Dispatcher.Invoke(() => // A UI szálon történő hívás szükséges
+                            {
+                                var gameWindow = new GameWindow(_client, _stream);
+                                gameWindow.Show();
+                                this.Close(); // A LayoutEditor ablakot bezárjuk
+                            });
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Dispatcher.Invoke(() =>
+                    {
+                        MessageBox.Show($"Hiba történt a szerver üzenetének fogadása közben: {ex.Message}");
+                    });
+                    break;
+                }
+            }
         }
     }
 }
